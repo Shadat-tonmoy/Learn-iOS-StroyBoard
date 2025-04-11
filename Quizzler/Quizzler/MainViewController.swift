630edf3e1f22cdea43ffd9b1e122aaa79aec0137
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2025-04-11 20:53:39 +0600

    - started with quizzler app

---
+++ Quizzler/Quizzler/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Shadat Mufakhkhar Tonmoy on 11/4/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Quizzler/QuizzlerTests/QuizzlerTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzlerTests.swift
+//  QuizzlerTests
+//
+//  Created by Shadat Mufakhkhar Tonmoy on 11/4/25.
+//
+
+import Testing
+@testable import Quizzler
+
+struct QuizzlerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
